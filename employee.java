c89c13cda2df061e4f317fdac459106c856fae88
Shiv <123@gmail.com>
2025-11-19 19:56:38 +0530

    Commited 1st time

---
+++ employee.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class employee{
+public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Shivang");
+	System.out.println(123);
+	System.out.println("IT");
+ }
+}	
+
+System.out,println(Hello baby)
